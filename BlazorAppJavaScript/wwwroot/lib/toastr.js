7d3f2af4b2914853b6e0dc06582566cd311228d1
Edier Bernal Rozo <ebernal@tibagroup.com>
2025-03-04 16:29:13 -0500

    Add project files.

---
+++ BlazorAppJavaScript/wwwroot/lib/toastr.js
@@ -0,0 +1,25 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") toastr.success(message)
+    else if (type === "error") toastr.error(message)
+    else if (type === "warning") toastr.warning(message)
+    else if (type === "info") toastr.info(message)
+    
+}
+
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-bottom-center",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
